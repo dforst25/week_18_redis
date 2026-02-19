c78119004ea2668740a8e88a32b3065192ec89a5
David Forst <dforst@g.jct.ac.il>
2026-02-19 11:11:42 +0200

    integration the moduls of the consumer

---
+++ consumer/main.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from redis_connection import RedisManager
+from mongo_connection import MongoConnection
+import json
+
+
+def main():
+    redis_cnx = RedisManager()
+    mongo_cnx = MongoConnection()
+    print("waiting for alerts...")
+    while True:
+        alert = redis_cnx.pop_alert('urgent_queue')
+        if alert is None:
+            alert = redis_cnx.pop_alert('normal_queue')
+        if alert is None:
+            continue
+        alert_json_format = json.loads(alert)
+        print(f"Got an {alert_json_format['priority']} alert:")
+        alert_json_format["insertion_time"] = datetime.now()
+        print(f"insertion_time: {alert_json_format['insertion_time']}")
+        mongo_cnx.insert_alert(alert_json_format)
+
+
+if __name__ == "__main__":
+    main()
